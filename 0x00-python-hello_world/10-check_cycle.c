7ce64505db64de1e59821508abea8d4bd1e8988e
Dan9el8 <mutetemifloice@gmail.com>
2023-10-02 06:19:56 -0700

    hello world

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+*check_cycle - a file that check list cycle
+*@list: list
+*
+*Return: 1 if cycle 0 if no
+*/
+
+int check_cycle(listint_t *list)
+{
+listint_t *slow = list;
+listint_t *fast = list;
+
+if(!list)
+return (0);
+
+while (slow && fast && fast->next)
+{
+slow = slow->next;
+fast = fast->next->next;
+if (slow == fast)
+return (1);
+}
+return (0);
+}
+
